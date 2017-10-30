d357a10c910dabc6764f8ca0c9fb5d71326d8fdb
Edgar Skobichevskiy <4_ipx@mail.ru>
2017-10-30 17:46:51 +0800

    fireBase integration

--- LKS/Views/ranksTableViewController.swift
+++ LKS/Views/ranksTableViewController.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
-class ranksTableViewController: UIViewController {
+class ranksTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
