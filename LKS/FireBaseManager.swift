74a90ee2314db84ce8e91e85db659cd4e1a817bc
Edgar Skobichevskiy <4_ipx@mail.ru>
2017-10-31 13:28:24 +0800

    add crew class

---
+++ LKS/FireBaseManager.swift
@@ -0,0 +1,27 @@
+//
+//  FireBaseManager.swift
+//  LKS
+//
+//  Created by Edgar Skobichevskiy on 31/10/2017.
+//  Copyright © 2017 Serhii Kyrychenko. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FBManager {
+    func createCrew()
+    {
+        
+    }
+    
+    func updateInfo()
+    {
+        
+    }
+    
+    func getInfoFromCategory()
+    {
+        
+    }
+}
